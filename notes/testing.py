922edf0cf405477284e95c292cecf5cbd37609d3
michael-maple-waffles <michael.willes@ucas-edu.net>
2026-02-10 04:36:59 +0000

    that was actually a fun project!

---
+++ notes/testing.py
@@ -0,0 +1,7 @@
+
+dictionary = {
+    '1' : ('Cats', 'Arnold Shwartsenager'),
+    '2' : ('Cats', 'Arnold Shwartsenager'),
+    '3' : ('Cats', 'Arnold Shwartsenager')
+}
+input(for item in dictionary.keys: f"Press {item} to remove {dictionary[item][0]}")

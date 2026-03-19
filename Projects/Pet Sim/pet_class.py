342f7e7437c77304ca66f10006919b923313827e
michael-maple-waffles <michael.willes@ucas-edu.net>
2026-03-19 10:49:08 -0600

    mehhhhhhhhhhhhhhhhhhhhhhhhhh

--- Projects/Pet Sim/pet_class.py
+++ Projects/Pet Sim/pet_class.py
@@ -1,21 +1,35 @@
 #MW_CP2
 
 #class Pet has these values: name, animal, age, health, happiness, hunger, days_hungry, energy, sleeping, !!rating!!
-
-    #declair self
-
-    #function to set health
-    
-        #health should start at 100, then should be decreased by 5-10(based on how low they are in hunger) + 10 (for everyday going hungry). then health should be decreased by age (by month). then health should be decreased by 5-100(based on days gone without sleep)
-
+class Pet:
+    def __init__(self, name, animal, age, happiness, hunger, energy, sleeping):
+        #declair self
+        self.name = name
+        self.animal = animal
+        self.age = age
+        self.happiness = happiness
+        self.hunger = hunger
+        self.energy = energy
+        self.sleeping = sleeping
     #function to decide happiness 
+    def happinessTracker(self):
+        #happiness will be decreased based on  hunger and energy
+        if self.hunger >= 90:
+            self.happiness += 10
+        elif self.hunger >= 75:
+            self.happiness += 5
+        elif self.hunger <= 10:
+            self.happiness -= 10
+        elif self.hunger <= 25:
+            self.happiness -= 5
 
-        #happiness will be decreased based on  
+    def happinessChange(self, change=0):
+        self.happiness += change
 
     #function to show pets current stats
+    def showStats(self):
+        
 
     #function to put pet to sleep
 
     #function to feed pet
-
-    #!!function to train pet!!

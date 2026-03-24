53fc929d72fdff89c36dcad785e362ff1b1ea6dd
michael-maple-waffles <michael.willes@ucas-edu.net>
2026-03-24 10:49:04 -0600

    fixed

--- notes/class_relationships.py
+++ notes/class_relationships.py
@@ -1,6 +1,6 @@
 #MW_CP2 INHERITANCE
 
-#inheritance:
+#inheritance: (is a)
 
 #parent
 class Vehicle:
@@ -13,11 +13,73 @@ class Vehicle:
 
     def move(self):
         print("Vroom!")
+#composition
+class Engine:
+    def __init__(self, cylinders):
+        self.cylinders = cylinders
+
+    def __str__(self):
+        return self.cylinders
 
 class Car(Vehicle):
-    pass
+    def __init__(self, brand, model):
+        super().__init__(brand, model)
+        self.engine = Engine("V8")
+
+class Plane(Vehicle):
+    def move(self):
+        print("Flyyyy!")
+
+class Boat(Vehicle):
+    def move(self):
+        print("sails!")
 
 car = Car('Ford', 'Mustange')
 
-car.show_vehicle()
-car.move()
+boat = Boat("Boom Shock", "WireFine")
+
+plane = Plane("Boeing", "747")
+
+
+for x in (car, boat, plane):
+    x.show_vehicle()
+    x.move()
+
+print(car.engine)
+#aggregation (has a--> ONE WAY)
+class Library:
+    def __init__(self, name, catalogue = []):
+        self.name = name
+        self.catalogue = catalogue
+
+    def addBook(self, book):
+        self.catalogue.append(book)
+
+    def removeBook(self, book):
+        if book in self.catalogue:
+            self.catalogue.pop(book)
+        else:
+            print("book doesn't exist")
+    
+    def veiwBook(self):
+        for book in self.catalogue:
+            print(book)
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+lib = Library("provo library")
+
+lib.addBook(Book("The Way of Kings", "Brandon Sanderson"))
+lib.addBook(Book("The Words of Radiance", "Brandon Sanderson"))
+lib.addBook(Book("Mistborn", "Brandon Sanderson"))
+
+lib.veiwBook()
+
+
+#composition (has a--> two way... they are dependant!

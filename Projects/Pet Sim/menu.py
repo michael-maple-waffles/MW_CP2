0a618cf5243913b957f4a7292794abbbd7852f41
michael-maple-waffles <michael.willes@ucas-edu.net>
2026-03-28 04:55:52 +0000

    mehhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- Projects/Pet Sim/menu.py
+++ Projects/Pet Sim/menu.py
@@ -1,1 +1,179 @@
 #MW_CP2 Menu and other functions put together
+from pet_class import Pet, choiceInput, intInput,floatInput
+import random
+import csv
+
+
+#function called create pet
+def createPet(all_pets=[]):
+#first ask what the pets name is, what species they are, and how old they are (in months)
+    while True:
+        name = input("\nWhat is the name of your pet?").title().strip()
+        if name in all_pets:
+            print("\nThat name is taken!")
+        else:
+            break
+    species = input("\nWhat is the species of your pet?").title().strip()
+    age = intInput(max = 100, min = 0, prompt = "\nHow old is your pet?")
+    return (f'{name}',Pet(name, species, age))
+
+#then return the information gathered, this should be set as "current pet"
+
+
+def event(current_pet):
+#function called event:
+    #make a random choice between 4 events
+    choice = random.randint(1,4)
+    #choice one is your pet made a new friend +20 happiness and +10 health
+    if choice == 1:
+        print("\nYour pet met a new friend! (+20 happiness and +10 health)")
+        current_pet.happiness += 20
+        current_pet.health += 10
+    #choice two is your pet got injured! -10 to health -20 to energy.
+    elif choice == 2:
+        print("\nYour pet got injured! (-10 health and -20 energy)")
+        current_pet.health -=10
+        current_pet.energy -= 20
+
+    #choice three is your pet took an extra nap +30 energy
+    elif choice == 3:
+        print("\nYour pet took and extra nap! (+30 energy)")
+        current_pet.energy += 30
+    #choice four is pet found a small animal and ate it! + 25 to hunger, -2 to health
+    elif choice == 4:
+        print("\nYour pet ate a small animal!? (+25 to hunger and -2 to health)")
+
+#function called save pet:
+def save_pet(previous_data={}, new_data={}):
+#get information to write,
+#dict info = {}
+    merged_dicts = previous_data | new_data
+    dict_info = []
+#for pet in pet dict:
+    for pet in merged_dicts.keys():
+        dict_info.append({"Name" : merged_dicts[pet].name, "Species" : merged_dicts[pet].species, "Age" : merged_dicts[pet].age, "Health" : merged_dicts[pet].health, "Happiness" : merged_dicts[pet].happiness, "Hunger" : merged_dicts[pet].hunger, "Energy":merged_dicts[pet].energy})
+#dict_info.append{Name : current_pet.name, species : current_pet.species, Age : current_pet.age, health : current_pet.health, Happiness : current_pet.happiness, Hunger : current_pet.hunger, Energy:current_pet.energy, sleeping : current_pet.sleeping}
+#fieldnames = ["Name","Species","Age","Health","Happiness","Hunger","Energy","Sleeping"]
+    fieldnames = ["Name","Species","Age","Health","Happiness","Hunger","Energy"]
+    #with open Projects/Pet Sim/saved_pet.csv 'w' as saving pet:
+    try:
+        with open("Projects/Pet Sim/saved_pet.csv", 'w') as saving_pets:
+    #saver = csv.dictwriter(saving_pet, fieldnames=fieldnames)
+            saver = csv.DictWriter(saving_pets, fieldnames=fieldnames)
+    #saver.writeheader
+            saver.writeheader()
+    #saver.writerows(dict_info)
+            saver.writerows(dict_info)
+    except:
+        print("\n\noops file failed\n\n")
+    else:
+        print("\nFile accessed successfully")
+
+#function called load pets:
+def loadPets():
+    #data = []
+    data = []
+    #sendable data = {}
+    sendable_data = {}
+    #with open Projects/Pet Sim/saved_pet.csv 'r' as loading pet:
+    try:
+        with open("Projects/Pet Sim/saved_pet.csv", 'r') as loading_pets:
+        #reader = csv.DictReader(loading pet)
+            reader = csv.DictReader(loading_pets)
+        #for row in reader:
+            for row in reader:
+            #data.append row
+                data.append(row)
+    except:
+        print("\n\noops file failed\n\n")
+    else:
+        print("\nFile accessed successfully")
+
+    #current pet = 0
+    current_pet = 0
+    #for item in data:
+    for item in data:
+    #sendabledata[[currentpet]['Name']] = Pet(data[currentpet]['Name'], data[currentpet]['Species'], data[currentpet]['Age'],data[currentpet]['Health'],data[currentpet]['Happiness'],data[currentpet]['Hunger'],data[currentpet]['Energy'],data[currentpet]['Sleeping'])
+        sendable_data[data[current_pet]['Name']] = Pet(data[current_pet]['Name'],data[current_pet]['Species'],data[current_pet]['Age'],data[current_pet]['Health'],data[current_pet]['Happiness'],data[current_pet]['Hunger'],data[current_pet]['Energy'])
+    #currentpet +=1
+        current_pet += 1
+    #return()
+    return sendable_data
+
+#function called run:
+def run():
+    current_pets = {}
+    current_pet = ""
+#while True
+    while True:
+#if user has a curent pet:
+        if current_pet == True:
+    #ask user if they would like to play with the pet, feed the pet, veiw the pets stats, put the pet to sleep, check pets status, manage pets, save game, or quit
+            choice = choiceInput(['1','2','3','4','5','6','7','8'], "\nYou currently have a pet selected!\nPress 1 to feed pet\n Press 2 to play with pet\n Press 3 to let pet sleep\n Press 4 to veiw pets stats\n Press 5 manage pets\n Press 6 to load game (will delete any unsaved progress)\n Press 7 to save game (will delete any unloaded data), or Press 8 to quite: : ")
+            if choice =='1':
+                current_pets[current_pet].feed()
+            elif choice == '2':
+                current_pets[current_pet].play()
+            elif choice == '3':
+                current_pets[current_pet].sleep()
+            elif choice == '4':
+                current_pets[current_pet].veiwPet()
+            elif choice == '5':
+                pass
+            elif choice == '6':
+                current_pets = loadPets()
+                if current_pet not in current_pets.keys():
+                    current_pet = ''
+            elif choice == '7':
+                save_pet(new_data=current_pets)
+            elif choice == '8':
+                break
+#elif user currently has a pet in pet library:
+        elif current_pet == False and bool(current_pets) == True:
+            choice = choiceInput(['1','2','3','4','5','6'], "\nYou currently don't have a pet selected!\nPress 1 to make a new pet\n Press 2 veiw all currently made pets\n Press 3 to select a pet\n Press 4 to load the current save file (will delete anyn currently unsaved pets)\n Press 5 to save your currently made pets\n or Press 6 to quite: ")
+            if choice == '1':
+                new_pet = createPet(current_pets.keys)
+                current_pets[new_pet[0]] = new_pet[1]
+            elif choice == '2':
+                for pet in current_pets.keys():
+                    print(current_pets[pet])
+            elif choice == '3':
+                while True:
+                    print("\nPlease type the name of one of these pets EXACTLY as written to select one")
+                    for pet in current_pets.keys():
+                        print(current_pets[pet])
+                    pet_pick = input('\nEnter Here: ')
+                    if pet_pick in current_pets.keys():
+                        current_pet = pet_pick
+                    else:
+                        print("Invalid name")
+            elif choice == '4':
+                current_pets = loadPets()
+            elif choice == '5':
+                save_pet(loadPets(), current_pets)
+            elif choice == '6':
+                break
+            else:
+                print("\nError code 1\n")
+                    
+                
+                
+    #show you don't currently have a pet picked!
+    #show pets and in a way that the user can pick
+    #allow the user to save.
+    #allow the user to quit
+#else:
+        else:
+    #ask the user if they would like to load game, make a new pet (overwriting save data), or quite
+            choice = choiceInput(['1','2','3'], "Hello!\nwelcome to the pet simulator!\nPress 1 to make a new pet\n Press 2 to load the current save file\n or Press 3 to close out: ")
+            if choice == '1':
+                new_pet = createPet(current_pets)
+                current_pets[new_pet[0]] = new_pet[1]
+            elif choice == '2':
+                current_pets = loadPets()
+            elif choice == '3':
+                break
+            else:
+                print("\nError code 1\n")
+
+run()
--- Projects/Pet Sim/pet_class.py
+++ Projects/Pet Sim/pet_class.py
@@ -1,35 +1,162 @@
 #MW_CP2
 
+
+import random
 #class Pet has these values: name, animal, age, health, happiness, hunger, days_hungry, energy, sleeping, !!rating!!
+def userInput(prompt = '> '):
+    return input(prompt).lower().strip()
+
+def intInput(max = 100000,prompt='> ',min = 0):
+    while True:
+        num = userInput(prompt)
+        try:
+            num = int(num)
+        except:
+            print('\nInput is not a number!')
+            continue
+        if num <= max and num >= min:
+            return num
+        else:
+            print('\nInput is out of range!')
+
+def floatInput(max = 100000.00,prompt='> ',min = 0.00):
+    while True:
+        num = userInput(prompt)
+        try:
+            num = float(num)
+        except:
+            print('\nInput is not a number!')
+            continue
+        if num <= max and num >= min:
+            return num
+        else:
+            print('\nInput is out of range!')
+
+def choiceInput(choices,prompt = '> '):
+    while True:
+        choice = userInput(prompt)
+        if choice in choices:
+            return choice
+        else:
+            print('\nPlease select a valid choice!')
+
 class Pet:
-    def __init__(self, name, animal, age, happiness, hunger, energy, sleeping):
+    def __init__(self, name, animal, age, health=100, happiness=50, hunger=100, level=0, energy=100):
         #declair self
         self.name = name
         self.animal = animal
         self.age = age
+        self.health = health
         self.happiness = happiness
         self.hunger = hunger
-        self.energy = energy
-        self.sleeping = sleeping
-    #function to decide happiness 
-    def happinessTracker(self):
+        self.level = level
+        self.energy = energy    #function to decide happiness
+
+    def healthTracker(self, reason = "hunger", amount=0):
+        if reason == "hunger":
+            if self.hunger >= 90:
+                self.Health += 3
+            elif self.hunger >= 75:
+                self.Health += 1
+            elif self.hunger <= 10:
+                self.Health -= 5
+            elif self.hunger <= 25:
+                self.Health -= 2
+            else:
+                pass
+        
+        elif reason == "over worked":
+            self.Health -= 2
+
+        else:
+            self.Health += amount
+
+        if self.Health > 100:
+            self.Health = 100
+        elif self.Health < 0:
+            self.Health = 0
+
+    def happinessTracker(self, reason="hunger", amount=0):
         #happiness will be decreased based on  hunger and energy
-        if self.hunger >= 90:
-            self.happiness += 10
-        elif self.hunger >= 75:
-            self.happiness += 5
-        elif self.hunger <= 10:
+        if reason == "hunger":
+            if self.hunger >= 90:
+                self.happiness += 10
+            elif self.hunger >= 75:
+                self.happiness += 5
+            elif self.hunger <= 10:
+                self.happiness -= 10
+            elif self.hunger <= 25:
+                self.happiness -= 5
+            else:
+                pass
+        
+        elif reason == "over worked":
             self.happiness -= 10
-        elif self.hunger <= 25:
-            self.happiness -= 5
 
-    def happinessChange(self, change=0):
-        self.happiness += change
+        else:
+            self.happiness += amount
 
-    #function to show pets current stats
+        if self.happiness > 100:
+            self.happiness = 100
+        elif self.happiness < 0:
+            self.happiness = 0
+
+    #function to display stats:
     def showStats(self):
+        return f"{self.name} | Happiness is currently {self.happiness}/100 | Hunger is currently {self.hunger}/100 | Energy is currently {self.energy}"
+    
+    #function for play:
+    def play(self):
+        if self.level == 0:
+            print("\nYour pet ran in circles!")
+        elif self.level == 1:
+            print(random.choice("\nYour pet ran in circles!", "\nYour pet did a barrel roll!"))
+        elif self.level == 2:
+            print(random.choice("\nYour pet ran in circles!", "\nYour pet did a barrel roll!", "\nYour pet did a back flip!"))
+        elif self.level >=3:
+            print(random.choice("\nYour pet ran in circles!", "\nYour pet did a barrel roll!", "\nYour pet did a back flip!", "\nYour pet behaved perfectly!"))
+        self.happiness += 20
+        self.energy -= 30
+        self.happinessTracker()
+        if self.energy > 20:
+            print("\n your pet is over worked!")
+            self.happinessTracker(reason = "over worked")
+        elif self.energy <= 50:
+            print(f"your pet is starting to feel tired! current energy = {self.energy}")
+
+        if self.energy < 0:
+            self. energy = 0
+
+        self.happinessTracker("NULL")
+        
         
 
     #function to put pet to sleep
+    def sleep(self):
+        self.energy += 100
 
     #function to feed pet
+    def feed(self):
+        choice = choiceInput(['1','2','3'], '\nPress 1 if you would like to give your pet Normal Chow (easy to eat good fuel), Press 2 if you would like to give your pet Hungy Chow (refills hunger greatly, is exhuasting to eat), Press 3 if you would like to give your pet Speed Chow (easy to eat, not filing)')
+        if choice == '1':
+            self.hunger += 50
+            self.energy -= 20
+        elif choice == '2':
+            self.hunger += 75
+            self.energy -= 30
+        elif choice == '3':
+            self.hunger += 25
+            self.energy -= 10
+
+        self.happinessTracker()
+
+        if self.energy <= 20:
+            print("\n Your pet is overworked!")
+            self.happinessTracker(reason="over worked")
+        if self.energy < 0:
+            self.energy = 0
+        if self.hunger > 100:
+            self.hunger = 100        
+    #str function that just shows the pets name, age, and animal
+    def __str__(self):
+        return f"{self.name}, is a {self.animal}, who is {self.age} years old"
